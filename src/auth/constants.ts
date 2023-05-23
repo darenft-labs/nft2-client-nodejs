5edcc7ec1df0b1caba590a6f16e8e80c811e34e6
vjtc0n <vjtc0n12345@gmail.com>
2023-05-23 09:56:48 +0700

    Update pretestnet env

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,11 +1,13 @@
 export enum ChainType {
   STAGING = 1,
   TEST_NET = 2,
-  MAIN_NET = 3,
+  PRE_TEST_NET = 3,
+  MAIN_NET = 4,
 }
 
 export const HOST_URL = {
   [ChainType.STAGING]: 'https://protocol-stg.dareplay.io',
+  [ChainType.PRE_TEST_NET]: 'https://protocol-api-pretest.dareplay.io',
   [ChainType.TEST_NET]: 'https://protocol-api-testnet.dareplay.io',
   [ChainType.MAIN_NET]: '',
 };
